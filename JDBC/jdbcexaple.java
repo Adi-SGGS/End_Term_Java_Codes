b04d17ce4fc93c3e71f528dd6d151c5eee5edc19
Adi-SGGS <97872895+Adi-SGGS@users.noreply.github.com>
2022-12-13 01:43:41 +0530

    Update jdbcexaple.java

--- JDBC/jdbcexaple.java
+++ JDBC/jdbcexaple.java
@@ -7,11 +7,20 @@ public static class jdbcexample {
         try{ 
             // load JDBC Driver
             Class.forName("com.mysql.jdbc.Driver");  
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost3000:/username","root","root");  
+            // Opening the connection
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost3000:/username","root","root");  
             //here 'username' is database name, 'root' is username and password  
+            
+            // Creating a statment
             Statement stmt = con.createStatement();  
-            ResultSet rs=stmt.executeQuery("CREATE TABLE studrnts;");  
+            
+            // Execute the statment
+            ResultSet rs = stmt.executeQuery("CREATE TABLE studrnts;");  
+            
             /* Code to be executed after querry */
+            
+            // closing the connection
+            con.close();
         }catch(Exception e){ System.out.println(e);} 
     } 
 } 
